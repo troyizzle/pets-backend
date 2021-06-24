52a63d7e2e6a49ccd7219443067577761fe1d2b7
Troy Underwood <troyizzle@gmail.com>
2021-06-24 15:52:48 -0400

    Update rejecting unaothirzed connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,10 +10,11 @@ module ApplicationCable
 
     # @return [User]
     def find_verified_user
-      reject_unauthorized_connection unless cookies[:token] && cookies[:token].split(' ').size > 1
+      reject_unauthorized_connection unless cookies[:token]
 
       # On production the token seems to be parsing the spacing
       split_by = cookies[:token].include?(' ') ? ' ' : '%20'
+      reject_unauthorized_connection unless cookies[:token].split(split_by).size > 1
 
       token = cookies[:token].split(split_by)[1]
       jwt = JWT.decode(token, Rails.application.credentials.devise_jwt[:secret_key], true, algorithm: 'HS256',
